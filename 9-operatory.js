5bfadc775c3a036c0b3b1287617fa8cfc1d9a5b9
Michał Jarząbek <nebraska994@gmail.com>
2017-06-10 14:28:36 +0200

    operatory

--- 9-operatory.js
+++ 9-operatory.js
@@ -1,2 +1,51 @@
 'use strict'
+//operatoty//
+/*
+* mnożenie
+/ dzielenie
++ dodawanie 
+- odejmowanie 
+% dzielenie modulo (reszta z dzielenia) 
+++ inkrementacja (zwiększanie) 
+-- dekrementacja (zmniejszanie)
+*/
+
+/* Operatyry Przypisania*/
+
+var liczba1 = 13, liczba2 = 5;
+
+var wynik;
+
+wynik= liczba1 % liczba2;
+console.log(wynik);
+
+var licz = 2;//licz= licz + 3;
+licz += 3;
+console.log(licz);
+
+/* Operatory porównania-służy do porównania argumętów*/
+/*
+== równe
+!= różne
+=== równa wartość i taki sam typ danych
+!== różne i inny typ danych
+> większe od
+< mniejsze od
+>= większe bądź równe
+od <= mniejsze bądź równe od
+*/
+var liczba1 = "13", liczba2 = 13;
+
+if (liczba1 == liczba2) {
+	console.log("równe")
+}
+
+/* Operatory logiczne: and, or, not*/
+
+var liczba1 = 13, liczba2 = 5;
+
+if ((liczba2 > 3) && (liczba2 < 10) ) {
+	console.log("tak")
+} 
+
 
